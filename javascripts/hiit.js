c27ad74764b0281867892180590b1d24e957a304
Jay <jfields@drw.com>
2017-04-17 10:16:04 -0400

    add timer

--- javascripts/hiit.js
+++ javascripts/hiit.js
@@ -19,3 +19,25 @@ scheduleSpeak(12 * minute, "Time for HIIT. Go HIIT Now. Go HIIT Now.");
 scheduleSpeak(21.5 * minute, "30 Seconds. Just 30 seconds till HIIT is done");
 scheduleSpeak(22 * minute, "Stop Stop STOP Stop Stop STOP Stop Stop");
 scheduleSpeak(32 * minute, "You're done. DONE. DONE. You're done");
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var display = document.querySelector('#time');
+    startTimer(32 * 60, display);
+};
